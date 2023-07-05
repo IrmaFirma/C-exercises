d6a54e43b0350144bdc8fcd06ea19373a334f9d1
Irma Preldzic <66248314+IrmaFirma@users.noreply.github.com>
2023-07-05 21:31:51 +0200

    Create paran.cpp

---
+++ paran.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    int a = 0;
+    cin>>a;
+    
+    if(a%2==0){
+        cout<<"Broj "<<a<<" je paran.";
+    }else{
+        cout<<"Broj "<<a<<" je neparan.";
+    }
+
+    return 0;
+}
