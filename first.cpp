3200047963ffa2291ed61dc98db3876502b244e4
Irma Preldzic <66248314+IrmaFirma@users.noreply.github.com>
2023-07-05 21:30:04 +0200

    Create first.cpp

---
+++ first.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    double a = 0;
+    cin>>a;
+    
+    if(a>10){
+        cout<<a*a;
+    }else{
+        cout<<a/4;
+    }
+
+    return 0;
+}
