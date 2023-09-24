546c4314f81438bc497227822f4ce3666c4aba14
Irma Preldzic <66248314+IrmaFirma@users.noreply.github.com>
2023-09-24 09:39:18 +0200

    Create zadatak1_vjezbe1.c

---
+++ zadatak1_vjezbe1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// Zadatak 1, Vjezbe 1
+
+int main(void) {
+  
+  int age;
+  printf("Koliko godina imas: ");
+  scanf("%d", &age);
+  printf("Ti si Irma Preldzic i imas %d godina :) \n", age);
+  
+  return 0;
+}
