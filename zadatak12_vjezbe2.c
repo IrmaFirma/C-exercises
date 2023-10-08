3158f3b84476afa8d2867b728054b254c2858203
Irma Preldzic <66248314+IrmaFirma@users.noreply.github.com>
2023-10-08 17:57:12 +0200

    Create zadatak12_vjezbe2.c

---
+++ zadatak12_vjezbe2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+// Zadatak 12, Vjezbe 2
+// %g sve nule ponistava u formatiranju
+
+
+int main(void) {
+
+  float x = 1.2340000, y = 1.00, z = 1.230;
+  printf("G formatirana 3 broja: %g , %g , %g", x, y, z);
+
+  
+  return 0;
+}
