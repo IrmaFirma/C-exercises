c4f143bc8b3fdb2cfde95ec4d7bcfe5c08f331e4
Irma Preldzic <66248314+IrmaFirma@users.noreply.github.com>
2023-10-08 17:40:07 +0200

    Create zadatak8_vjezbe2.c

---
+++ zadatak8_vjezbe2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+// Zadatak 8, Vjezbe 2
+// Sta ovaj kod radi?
+
+int main(void) {
+
+  int a; 
+  char b;
+  scanf("%d", &a);
+  scanf("%c", &b);
+  printf("%d", a);
+  printf("%c", b);
+
+  return 0;
+}
